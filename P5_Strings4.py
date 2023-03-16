97643f548fd2bfc138c580494dd5ec3922062b60
SergiHimself <124947141+SergiHimself@users.noreply.github.com>
2023-03-16 02:07:41 -0600

    Delete P5_Strings4.py

--- P5_Strings4.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 12 14:25:45 2023
-
-@author: sergi
-"""
-
-a = "- Python.\n- JavaScript.\n- Java.\n- PHP.\n- TypeScript\n- SQL.\n- COBOL.\n- C.\n- C++."
-print(a)
